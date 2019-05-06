8330ec03af08907d62fe9496dd2a6f4e9aede05b
Lorraine Wang <lorrainewang78@gmail.com>
2019-05-06 22:20:59 +0800

    1. Basic Routing

--- lesson1/loops/application.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    names = ["Alice", "Bob", "Charlie"]
-    return render_template("index.html", names=names)
