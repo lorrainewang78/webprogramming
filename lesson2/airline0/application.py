9cb34ed00cc7688cb54528e4b44da171dc51c545
lorrainewang78 <50280227+lorrainewang78@users.noreply.github.com>
2019-05-14 14:23:28 +0800

    Update application.py

--- lesson2/airline0/application.py
+++ lesson2/airline0/application.py
@@ -6,27 +6,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
-#DATABASE_URL = "postgresql+psycopg2://usygtmsgbejhsu:b06b1b8bafa4f4b194d415955b47a30ec34672fb44bbf70e490a5da45e6384ad@ec2-174-129-10-235.compute-1.amazonaws.com:5432/d1ceb2a8b68h58"
-#engine = create_engine(DATABASE_URL)
 engine = create_engine(os.getenv("DATABASE_URL"))
-#engine = psycopg2.connect(DATABASE_URL, sslmode='require')
 db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
     flights = db.execute("SELECT * FROM flights").fetchall()
     return render_template("index.html", flights=flights)
-    # try:
-    #
-    # except Exception as e:
-    #     print(e)
-    # #return "Nothing"
-
-@app.route("/showall")
-def showall():
-    flights = db.execute("SELECT * FROM flights").fetchall()
-    return render_template("index.html", flights=flights)
-
+    
 @app.route("/book", methods=["POST"])
 def book():
     """Book a flight."""
